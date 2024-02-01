83437c1170ec2d992a0e0ff602dde9dc88e7c9b8
André Carraro <andre.carraro@sga.pucminas.br>
2024-02-01 20:44:50 -0300

    Modificações na estrutura do projeto

---
+++ Startup.cs
@@ -0,0 +1,36 @@
+﻿using SPG.Intf.Data;
+
+namespace SPG.WebApi
+{
+    public class Startup(IConfiguration configuration)
+    {
+        public IConfiguration Configuration { get; } = configuration;
+
+        public void ConfigureServices(IServiceCollection services) 
+        {
+            services.AddDbContext<AppDbContext>(options => {
+                //options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddAutoMapper(typeof(Startup));
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+}
