5c2b386e0cce56028a1cc8a36a642725a3400f89
André Carraro <andre.carraro@totvs.com.br>
2024-02-24 22:39:25 -0300

    Ajuste na entidade de pessoa

--- SPG.Domain/Model/Person/PersonModel.cs
+++ SPG.Domain/Model/Person/PersonModel.cs
@@ -1,25 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SPG.Domain.Model
 {
-    public class PersonModel()
-    {
-        [Key]
-        public int Id { get; set; }
+  public class PersonModel()
+  {
+    [Key]
+    public int Id { get; set; }
 
-        [Required]
-        [StringLength(45)]
-        public string Cpf { get; set; } = string.Empty;
+    [Required]
+    [StringLength(45)]
+    public string Cpf { get; set; } = string.Empty;
 
-        [ForeignKey("UserId")]
-        public int UserId { get; set; }
+    [ForeignKey("User")]
+    public string UserId { get; set; } = string.Empty;
 
-        [Required]
-        [StringLength(200)]
-        public string Name { get; set; } = string.Empty;
+    public IdentityUser? User { get; set; }
 
-        [Required]
-        public DateOnly BirthDate { get; set; }
-    }
+    [Required]
+    [StringLength(200)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    public DateOnly BirthDate { get; set; }
+  }
 }
