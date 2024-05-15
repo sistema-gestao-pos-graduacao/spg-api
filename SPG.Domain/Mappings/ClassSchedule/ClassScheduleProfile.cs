97fc00919403a18b24bb691d685e051d6b97825c
André Carraro <andre.carraro@sga.pucminas.br>
2024-05-14 22:32:09 -0300

    Ajustes

--- SPG.Domain/Mappings/ClassSchedule/ClassScheduleProfile.cs
+++ SPG.Domain/Mappings/ClassSchedule/ClassScheduleProfile.cs
@@ -11,7 +11,7 @@ namespace SPG.Domain.Mappings
       CreateMap<ClassScheduleModel, ClassScheduleDto>()
         .ForMember(dest => dest.SubjectName, opt => opt.MapFrom(src => src.Subject != null ? src.Subject.Name : string.Empty))
         .ForMember(dest => dest.TeacherName, opt => opt.MapFrom(src => src.Subject != null && src.Subject.Teacher != null ? src.Subject.Teacher.Name : string.Empty))
-        .ForMember(dest => dest.TeacherId, opt => opt.MapFrom(src => src.Subject != null && src.Subject.Teacher != null ? src.Subject.Teacher.Id.ToString() : string.Empty))
+        .ForMember(dest => dest.TeacherId, opt => opt.MapFrom(src => src.Subject != null && src.Subject.Teacher != null ? src.Subject.Teacher.Id.ToString() : null))
         .ReverseMap()
         .ForMember(dest => dest.Subject, opt => opt.Ignore());
     }
